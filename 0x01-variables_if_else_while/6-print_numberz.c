156cdeb67ab84b29cb479f2bc3acd24f4f7c22bc
abinejohn <chrisabine8@gmail.com>
2022-10-17 17:35:22 -0700

    c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digits numbers of base 10
- * You are not allowed to use any variable of typ char
- * You can only use putchar twice in your code 
+ * main - prints all single digit numbers of base 10
+ * You are not allowed to use any variable of type char
+ * You can only use putchar twice in your code
  * All your code should be in the main f
  * Return: Always success
  */
