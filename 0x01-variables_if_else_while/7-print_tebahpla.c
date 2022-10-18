40de50af3c56d3b1923681dfe3f65f17185bc04d
abinejohn <chrisabine8@gmail.com>
2022-10-17 17:12:52 -0700

    C

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main prints the lowercase alphabet in reverse
+ * main - prints the lowercase alphabet in reverse
  * You can only use the putchar function
  * All your code should be in the main function
- * You can only use use putchar twice in your code
- * Return: Always success
+ * You can only use putchar twice in your code
+ * Return: Always Success
  */
 
 int main(void)
-{ 
+{
 	char gh;
 
-	for(gh = 'z'; gh >= 'a'; gh--)
+	for (gh = 'z'; gh >= 'a'; gh--)
 	{
 		putchar(gh);
 	}
