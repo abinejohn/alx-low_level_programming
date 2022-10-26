66a023d3fee057795e6822a7152499a526f2874b
abinejohn <chrisabine8@gmail.com>
2022-10-26 14:23:03 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+void puts2(char *str);
+void _puts(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+void print_rev(char *s);
+int _strlen(char *s);
+int _atoi(char *s);
+void rev_string(char *s);
+int _putchar(char c);
+void swap_int(int *a, int *b);
+char *_strcpy(char *dest, char *src);
+void reset_to_98(int *n);
+#endif
