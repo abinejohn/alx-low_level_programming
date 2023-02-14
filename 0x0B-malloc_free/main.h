6b4f37c6611d6f49cf5d7fac86e49c83bddcba60
abinejohn <chrisabine8@gmail.com>
2023-02-14 05:41:27 -0800

    0x0B. Malloc, Free

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+#endif /*MAIN_H*/
